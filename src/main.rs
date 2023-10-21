65a4df3ede00dd54154e8633e5657596bb718d52
pardeep yadav <pardeepyadav2914@gmail.com>
2023-10-21 22:08:25 +0530

    adding package and import it and create struct for task

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use actix_cors::Cors;
+
+use actix_web::{http, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+use serde::{Deserialize, Serialize};
+
+use reqwest::Client as HttpClient;
+
+use async_trait::async_trait;
+
+use std::sync::Mutex;
+
+use std::collections::HashMap;
+
+use std::fs;
+
+use std::io::Write;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Task {
+    id: u64,
+    name: String,
+    completed: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct User {
+    id: u64,
+    username: String,
+    password: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Database {
+    tasks: HashMap<u64, Task>,
+    users: HashMap<u64, User>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
